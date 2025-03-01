e716c9d5132d00865597048b20db29c09f95c52b
Shanmukha Appajosyula <149186397+Shanmukha2005@users.noreply.github.com>
2025-03-01 14:28:58 +0530

    Add files via upload

---
+++ fan.js
@@ -0,0 +1,26 @@
+let on = document.getElementById("btn1");
+let off = document.getElementById("btn2");
+let b1 = document.getElementById("btn3");
+let b2 = document.getElementById("btn4");
+let b3 = document.getElementById("btn5");
+let img = document.getElementById("image");
+
+function onbtn() {
+    img.style.animation = "rotate 4s infinite linear";
+}
+
+function offbtn() {
+    img.style.animation = "none";
+}
+
+function speed1btn() {
+    img.style.animation = "rotate 4s infinite linear";
+}
+
+function speed2btn() {
+    img.style.animation = "rotate 2s infinite linear";
+}
+
+function speed3btn() {
+    img.style.animation = "rotate 1s infinite linear";
+}
